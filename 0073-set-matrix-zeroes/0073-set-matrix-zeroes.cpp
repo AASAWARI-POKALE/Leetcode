f764e77acc4d53acce74f92a4287e8be73913ed8
AASAWARI-POKALE <asawaripokale@gmail.com>
2026-07-07 18:32:36 +0530

    Time: 0 ms (100%), Space: 21 MB (30.08%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    void markRow(int i, vector<vector<int>>& matrix) {
-    int n = matrix[0].size();
-    for (int j = 0; j < n; j++) {
-        if (matrix[i][j] != 0)
-            matrix[i][j] = -999;
-    }
-}
-   void markColumn(int i, vector<vector<int>>& matrix) {
-    int m = matrix.size();
-    for (int j = 0; j < m; j++) {
-        if (matrix[j][i] != 0)
-            matrix[j][i] = -999;
-    }
-}
+    
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix[0].size();
         int m = matrix.size();
+        vector<int> rows(m, 0);
+        vector<int> cols(n, 0);
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    markRow(i,matrix);
-                    markColumn(j,matrix);
+                    rows[i] = 1;
+                    cols[j] = 1;
                 }
             }
         }
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == -999) {
-                    matrix[i][j] = 0;
+                if (matrix[i][j] != 0) {
+                    if (rows[i] == 1 || cols[j] == 1) {
+                        matrix[i][j]=0;
+                    }
                 }
             }
         }
